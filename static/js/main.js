90d75e1c259c561f9bdda0a9ff1c76c24438452b
Pesh <peshwasm@stud.ntnu.no>
2021-03-25 11:50:44 +0100

    fixed some frontend

--- static/js/main.js
+++ static/js/main.js
@@ -53,8 +53,14 @@ class TypeWriter {
   function addComment() {
     var x = document.createElement("P")
     var y = document.getElementById("commentarea")
+    var name = document.createElement("P")
+    var date = document.createElement("P")
     var i = y.value
-    x.style.cssText ="position: float; display: block; text-content: center; border: 5px solid white; padding: 30px"
+    x.style.cssText ="position: relative; display: inline-block; text-content: center; border: 5px solid white; padding: 10px 30px 10px 30px; table-layout:fixed; margin: auto; text-align: none"
     x.append(i)
-    document.getElementById("commentsection").append(x)
+    name.append("Thomas")
+    date.append('10/11/1955 10:40:50 AM')
+    date.style.cssText = "color: red;"
+    document.getElementById("box_items1").append(x, name, date)
   }
+
