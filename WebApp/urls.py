742fcb0b985f0b481c07aac390b90e39e1626411
Hans Tjøtta <hahotjo@gmail.com>
2021-04-06 17:35:36 +0200

    fix urls

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -26,15 +26,14 @@ urlpatterns = [
     path('estates/<int:estate_id>/', views.view_estate, name='estate'),
     path('estates/<int:estate_id>/adminoverview/', views.admin_view_estate, name='estate.adminoverview'),
     path('estates/<int:estate_id>/<int:item_id>/notify/<int:user_id>/', views.notify, name='user_notify'),
-    path('estates/<int:estate_id>/end_estate/', TemplateView.as_view(template_name='estate/end_estate.html'), name='end_estate'),
-    path('status/<int:estate_id>/', views.status, name='status'),
+    #path('estates/<int:estate_id>/end_estate/', TemplateView.as_view(template_name='estate/end_estate.html'), name='end_estate'),
     path('estates/<int:estate_id>/<int:item_id>/addcomment', views.write_comment, name='estate_addcomment'),
     path('estates/<int:estate_id>/<int:item_id>/', views.show_item, name='show_item'),
 
     path('estates/<int:estate_id>/<int:item_id>/adminoverview', views.admin_view_item, name='admin_view_estate_item'),
-    path('estate_notfinished/<int:estate_id>/', views.admin_estate_notfinished, name='estate_notfinished'),
-    path('estate_item_finished/<int:estate_id>/<int:item_id>/', views.estate_item_finished, name='estate_item_finished'),
-    path('estate_finished/<int:estate_id>/', views.finish_estate, name='finish_estate'),
+    path('estate/<int:estate_id>/notfinished', views.admin_estate_notfinished, name='estate_notfinished'),
+    path('estate/<int:estate_id>/<int:item_id>/finish', views.estate_item_finished, name='estate_item_finished'),
+    path('estate/<int:estate_id>/finish', views.finish_estate, name='finish_estate'),
 ]
 
 if settings.DEBUG:
